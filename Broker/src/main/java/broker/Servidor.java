963fc0f7be9141292a874df6af06a3800669b9de
amse1029 <alexa.soto236348@potros.itson.edu.mx>
2023-12-06 23:05:13 -0700

    Creación de clases Broker

---
+++ Broker/src/main/java/broker/Servidor.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package broker;
+
+import dominio.FichaJugador;
+import dominio.Partida;
+import java.net.ServerSocket;
+
+/**
+ *
+ * @author alexa
+ */
+public class Servidor {
+    
+    private ServerSocket server;
+    
+    public void iniciar(Partida partida) {
+        
+    }
+    
+    public void enviarFicha(FichaJugador ficha) {
+        
+    }
+    
+    public void actPozo() {
+        
+    }
+    
+    public void actTablero() {
+        
+    }
+    
+    public void pasarTurno() {
+        
+    }
+}
