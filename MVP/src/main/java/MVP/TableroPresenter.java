ac75f23eeab8c8dded574e4e5669576aa1d79e20
Dect21 <dect2101@gmail.com>
2023-12-06 23:07:01 -0700

    Revert "Merge branch 'main' of https://github.com/amse1029/ProyectoDomino"
    This reverts commit 898d1eca47d7361b79dc0f95f3022e41b88ead32, reversing
    changes made to 3303967fb9d101d88188b070e41fa294b580d910.

--- MVP/src/main/java/MVP/TableroPresenter.java
+++ MVP/src/main/java/MVP/TableroPresenter.java
@@ -44,7 +44,7 @@ public class TableroPresenter implements ITablero{
 
     @Override
     public void colocaFicha() {
-        
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
